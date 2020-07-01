3d5a3b2cb879e6fa85a2cfc4ad208656f4a04239
Carice Poston <carice.cp@gmail.com>
2020-07-01 12:17:04 -0400

    Updates

--- Capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/ApiAccountDAO.java
+++ Capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/ApiAccountDAO.java
@@ -10,7 +10,6 @@ import org.springframework.web.client.RestTemplate;
 
 import com.techelevator.tenmo.dao.AccountDAO;
 import com.techelevator.tenmo.models.Account;
-import com.techelevator.tenmo.models.User;
 
 public class ApiAccountDAO implements AccountDAO {
 
