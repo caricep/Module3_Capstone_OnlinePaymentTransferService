494ecb0531de4b6b57c9f165a522eab80c5b9605
Carice Poston <carice.cp@gmail.com>
2020-07-01 10:31:10 -0400

    Updates

--- Capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDAO.java
+++ Capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDAO.java
@@ -1,6 +1,11 @@
 package com.techelevator.tenmo.dao;
 
+import java.util.List;
+
+import com.techelevator.tenmo.model.Account;
+
 public interface AccountDAO {
 
 	public double getAccountBalanceByUserId(Long userId);
+	public List<Account> getListOfUserAccounts();
 }
--- Capstone/tenmo-server/src/main/java/com/techelevator/tenmo/jdbc/JDBCAccountDAO.java
+++ Capstone/tenmo-server/src/main/java/com/techelevator/tenmo/jdbc/JDBCAccountDAO.java
@@ -1,9 +1,14 @@
 package com.techelevator.tenmo.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.tenmo.dao.AccountDAO;
+import com.techelevator.tenmo.model.Account;
 
 @Component
 public class JDBCAccountDAO implements AccountDAO {
@@ -18,6 +23,31 @@ public class JDBCAccountDAO implements AccountDAO {
 	public double getAccountBalanceByUserId(Long userId) {
 		double accountBalance = jdbcTemplate.queryForObject("SELECT balance FROM accounts WHERE user_id = ?", double.class, userId);
 		return accountBalance;
+	}
+
+	@Override
+	public List<Account> getListOfUserAccounts() {
+		List<Account> listOfUserAccounts = new ArrayList<Account>();
+		
+		String selectSql = "SELECT account_id, accounts.user_id, balance FROM accounts JOIN users ON accounts.user_id = users.user_id";
+		SqlRowSet rows = jdbcTemplate.queryForRowSet(selectSql);
+		
+		while (rows.next()) {
+			Account account = makeAccountFromRow(rows);
+			listOfUserAccounts.add(account);
+		}
+		
+		return listOfUserAccounts;
+	}
+	
+	private Account makeAccountFromRow(SqlRowSet rows) {
+		Account account = new Account();
+		
+		account.setAccountId(rows.getInt("account_id"));
+		account.setUserId(rows.getInt("user_id"));
+		account.setAccountBalance(rows.getDouble("balance"));
+		
+		return account;
 	}
 
 }
