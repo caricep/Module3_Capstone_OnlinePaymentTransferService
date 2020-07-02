41edca2f0f2ec28f151bdbbdcb1b2099640c6358
Carice Poston <carice.cp@gmail.com>
2020-07-02 10:52:05 -0400

    Updates

--- Capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ Capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -3,11 +3,9 @@ package com.techelevator.tenmo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techelevator.tenmo.dao.TransferDAO;
